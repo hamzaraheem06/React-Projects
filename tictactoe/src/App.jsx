504f5914d7b4951f5dfbb1a8084d1f256e6891bd
Hamza <hamzaraheem06@gmail.com>
2024-09-13 12:05:03 +0500

    add timeline in the game

--- tictactoe/src/App.jsx
+++ tictactoe/src/App.jsx
@@ -15,31 +15,25 @@ function Square({ value, onClick }) {
 }
 
 // this is the parent component that containes all the app, that would render on the  browser.
-function Board() {
+function Board({ xIsNext, squares, onPlay }) {
+  const squaresCopy = squares.slice();
   // this state containes an array that containes all the values of the respectives square. e.g squares[0] represents the value of the first square.
-  let [squares, setSquares] = useState(Array(9).fill(null));
-
-  let [xIsNext, setXIsNext] = useState(true);
-
   // this function handles the rendering of the X's and O's when a user clicks on the square.
   // to track which <Square> is clicked, we pass an i prop from the parent container as can be seen in line 37.
   function handleClick(i) {
-    let squaresCopy = squares.slice(0, squares.length); // this creates a copy of the original array, which will be used later to reset the game. I guess.
-
     // this checkes that if the square has already been filled or not. if the square is already filled it will exit from the function without doing anything.
     if (squaresCopy[i] !== null || calculateWinner(squares)) {
       return;
     }
 
     // the following if and else will alternate the turns.
+
     if (xIsNext) {
       squaresCopy[i] = "X";
     } else {
       squaresCopy[i] = "O";
     }
-    setSquares(squaresCopy); // this is called when we need to re-render value of the squares.
-
-    setXIsNext(!xIsNext); // this statement will continously flip the turn state
+    onPlay(squaresCopy); // this is called when we need to re-render value of the squares.
   }
 
   // this part work on displaying who is the winner
@@ -52,12 +46,12 @@ function Board() {
   } /* else if (winner === null) {
     status = `It is a draw`;
   } */ else {
-    status = `It is ${xIsNext ? "X" : "O"} \`s turn`;
+    status = `It is ${xIsNext ? "X" : "O"} \'s turn`;
   }
 
   return (
     <div className="flex flex-col gap-2 items-center">
-      <div className="text-4xl font-bold  text-blue-600">{status}</div>
+      <div className="text-4xl font-bold  text-white">{status}</div>
       <div className="flex flex-col overflow-hidden">
         <div className="flex">
           {/* the props are passed from here */}
@@ -147,4 +141,50 @@ function calculateWinner(squares) {
   return null;
 }
 
-export default Board; // this line exports the App so that it could be rendered in the UI using main.jsx file
+function Game() {
+  let [xIsNext, setXIsNext] = useState(true);
+  let [history, setHistory] = useState([Array(9).fill(null)]);
+  let [currentMove, setCurrentMove] = useState(0);
+  let currentSquares = history[currentMove];
+
+  function handlePlay(nextSquares) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+    setXIsNext(!xIsNext);
+  }
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+    setXIsNext(nextMove % 2 === 0);
+  }
+
+  let moves = history.map((square, move) => {
+    let description;
+    if (move > 0) {
+      description = "Go to move #" + move;
+    } else {
+      description = "Go to game start";
+    }
+    return (
+      <li key={move} className="pl-10 font-bold text-base font-mono">
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+
+  return (
+    <div className="flex gap-4 items-end justify-center">
+      <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+
+      <div className="w-72 h-72 bg-green-600 ">
+        <h1 className="text-3xl font-bold text-white text-center">
+          Game's History
+        </h1>
+        <ul className=" list-none pt-1">{moves}</ul>
+      </div>
+    </div>
+  );
+}
+
+export default Game; // this line exports the App so that it could be rendered in the UI using main.jsx file
